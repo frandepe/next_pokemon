445ce8ab6d0bd194aaa52ec8c52243aada3f335b
Franco De Paulo <frandepaulo@yahoo.com.ar>
2022-10-25 08:29:14 -0300

    first commit

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import React, { FC, ReactNode } from "react";
+import { Navbar } from "../ui";
+
+interface Props {
+  children?: ReactNode;
+  title?: string;
+}
+
+const origin = typeof window === "undefined" ? "" : window.location.origin;
+
+export const Layout: FC<Props> = ({ children, title }) => {
+  return (
+    <>
+      <Head>
+        <title>{title || "PokemonApp"}</title>
+        <meta name="author" content="Franco De Paulo" />
+        <meta
+          name="description"
+          content={`Información sobre el pokemon ${title}`}
+        />
+        <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+        <meta property="og:title" content={`Información sobre ${title}`} />
+        <meta
+          property="og:description"
+          content={`Esta es la página sobre ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
+      </Head>
+      <Navbar />
+      <main
+        style={{
+          padding: "0px 20px",
+        }}
+      >
+        {children}
+      </main>
+    </>
+  );
+};
